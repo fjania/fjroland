9c095c3f94227f0c0f74abdbcc0678d7912b010e
Frank Jania <fjania@findings.com>
2020-08-16 23:50:03 -0400

    comments and rework the stream opening

--- pkg/audio/waveform/samples.go
+++ pkg/audio/waveform/samples.go
@@ -33,15 +33,23 @@ func NewSamplePack(samplePackPath string) (*SamplePack, error) {
 		return nil, err
 	}
 
-	stream, err := portaudio.OpenDefaultStream(
-		0,
-		2,
-		44100.0,
-		portaudio.FramesPerBufferUnspecified,
-		samplePack.ProcessAudio,
-	)
+	// We'll use the default audio device and assume stereo wav files
+	// sampled at 44.1k. This could be changed, but all the samples
+	// provided conform, and it's a reasonable default.
+	defaultOutputDevice, err := portaudio.DefaultOutputDevice()
+	if err != nil {
+		log.Fatal("Failed to access the default audio device on this computer")
+		return nil, err
+	}
+	p := portaudio.LowLatencyParameters(nil, defaultOutputDevice)
+	p.Input.Channels = 0
+	p.Output.Channels = 2
+	p.SampleRate = 44100.0
+	p.FramesPerBuffer = portaudio.FramesPerBufferUnspecified
+	stream, err := portaudio.OpenStream(p, samplePack.ProcessAudio)
 
 	if err != nil {
+		log.Fatal("Failed to open a stream to the default audio device on this computer")
 		return nil, err
 	}
 
@@ -51,7 +59,8 @@ func NewSamplePack(samplePackPath string) (*SamplePack, error) {
 
 func LoadSamplePack(samplePackPath string) (*SamplePack, error) {
 	k := &SamplePack{
-		Samples: make(map[string]*Sample),
+		Samples:        make(map[string]*Sample),
+		SamplePackPath: samplePackPath,
 	}
 
 	files, err := ioutil.ReadDir(samplePackPath)
@@ -79,26 +88,7 @@ func LoadSamplePack(samplePackPath string) (*SamplePack, error) {
 	}
 	k.Instruments = instruments
 
-	k.SamplePackPath = samplePackPath
-
 	return k, nil
-}
-
-// Implement the audio Output interface
-func (k *SamplePack) Name() string {
-	return k.SamplePackPath
-}
-
-func (k *SamplePack) Play(instrument string, level float32) {
-	k.Samples[instrument].Play(level)
-}
-
-func (k *SamplePack) ListInstruments() map[string]bool {
-	return k.Instruments
-}
-
-func (k *SamplePack) HasInstrument(instrument string) bool {
-	return k.Instruments[instrument]
 }
 
 func LoadSample(filepath string) (*Sample, error) {
@@ -131,10 +121,21 @@ func LoadSample(filepath string) (*Sample, error) {
 }
 
 func (s *Sample) Play(level float32) {
+	// Input levels are 1, 2, 3. To make them sound different to the human
+	// ear we'll square the level to make the volume the sample should be
+	// played at.
 	s.Volume = level * level
 	s.Playhead = 0
 }
 
+// Use the "playhead" method of playing back the samples. Setting the
+// playhead to 0 means that we'll loop back trough the sample's data and
+// add that to the []float32 which portaudio uses to write to the audio
+// device on the computer. We do this for every sample that should be
+// played, and clamp the output at 1.0 to avoid clipping.
+//
+// Found this method of playing back the audio in this repo
+// https://github.com/kellydunn/go-step-sequencer
 func (k *SamplePack) ProcessAudio(out []float32) {
 	for i := range out {
 		var data float32
@@ -151,4 +152,21 @@ func (k *SamplePack) ProcessAudio(out []float32) {
 
 		out[i] = data
 	}
+}
+
+// Implement the AudioOutput interface
+func (k *SamplePack) Name() string {
+	return k.SamplePackPath
+}
+
+func (k *SamplePack) Play(instrument string, level float32) {
+	k.Samples[instrument].Play(level)
+}
+
+func (k *SamplePack) ListInstruments() map[string]bool {
+	return k.Instruments
+}
+
+func (k *SamplePack) HasInstrument(instrument string) bool {
+	return k.Instruments[instrument]
 }
