5ebf834d8ea3b294c9f99af1194925e31574d5bf
Frank Jania <fjania@findings.com>
2020-08-16 22:32:26 -0400

    comment / refactor

--- pkg/audio/midi/midi.go
+++ pkg/audio/midi/midi.go
@@ -3,7 +3,6 @@ package audio
 import (
 	"github.com/rakyll/portmidi"
 	"log"
-	"time"
 )
 
 type Instrument struct {
@@ -18,6 +17,13 @@ type Midi struct {
 	OutputStream *portmidi.Stream
 }
 
+const (
+	MidiPercussionChannel = 0x9A
+)
+
+// A subset of the percussive instruments available on midi channel 10.
+// There's no reason we couldn't support all of them, but this is a fine
+// place to start for this example.
 var instruments = [15]Instrument{
 	Instrument{"Bass", 35},
 	Instrument{"Cross-stick", 37},
@@ -36,25 +42,6 @@ var instruments = [15]Instrument{
 	Instrument{"Cowbell", 56},
 }
 
-// AudioOutput Interface Methods
-func (m *Midi) Name() string {
-	return m.DeviceName
-}
-func (m *Midi) Play(instrument string, level float32) {
-	// input levels are 1, 2, 3 well map them linearly
-	// as 40, 80 12
-	midiNote := m.MidiNotes[instrument]
-	m.OutputStream.WriteShort(0x9A, int64(midiNote), int64(level*40))
-}
-
-func (m *Midi) ListInstruments() map[string]bool {
-	return m.Instruments
-}
-
-func (m *Midi) HasInstrument(instrument string) bool {
-	return m.Instruments[instrument]
-}
-
 func NewMidi(deviceName string) (*Midi, error) {
 	m := &Midi{
 		DeviceName:  deviceName,
@@ -69,14 +56,15 @@ func NewMidi(deviceName string) (*Midi, error) {
 
 	portmidi.Initialize()
 
+	// There isn't a map of devices we can use, so we'll loop through
+	// all the devices trying to find the id of the one we were told
+	// to use.
 	var deviceID portmidi.DeviceID
 
 	for i := 0; i < portmidi.CountDevices(); i++ {
 		info := portmidi.Info(portmidi.DeviceID(i))
 		if info.IsOutputAvailable && info.Name == deviceName {
 			deviceID = portmidi.DeviceID(i)
-			info := portmidi.Info(portmidi.DeviceID(i))
-			log.Printf("Device ID:%d Info> %+v\n", deviceID, info)
 		}
 	}
 
@@ -86,21 +74,24 @@ func NewMidi(deviceName string) (*Midi, error) {
 		log.Printf("Failed to open Midi Device: '%v'", deviceName)
 		return nil, streamErr
 	}
-	log.Printf("Stream Opened Device ID:%d > %+v\n", deviceID, portmidi.Info(deviceID))
 
 	return m, nil
 }
 
-func (m *Midi) demo() {
-	// Send "note on" events to play C major chord.
-	for i := 0; i < 120; i++ {
-		m.Play("Closed Hi-Hat", 2)
-		time.Sleep(time.Second / 8)
-		m.Play("Clap", 2)
-		time.Sleep(time.Second / 8)
-		m.Play("Snare", 2)
-		time.Sleep(time.Second / 8)
-		m.Play("Bass", 2)
-		time.Sleep(time.Second / 8)
-	}
+// AudioOutput Interface Methods
+func (m *Midi) Name() string {
+	return m.DeviceName
+}
+func (m *Midi) Play(instrument string, level float32) {
+	// Input levels are 1, 2, 3 well map them linearly as 40, 80 12
+	midiNote := m.MidiNotes[instrument]
+	m.OutputStream.WriteShort(MidiPercussionChannel, int64(midiNote), int64(level*40))
+}
+
+func (m *Midi) ListInstruments() map[string]bool {
+	return m.Instruments
+}
+
+func (m *Midi) HasInstrument(instrument string) bool {
+	return m.Instruments[instrument]
 }
